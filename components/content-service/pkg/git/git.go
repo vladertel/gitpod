435dd5398e0a4c62bc010116b4066b2195551b0d
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2022-10-13 13:07:44 -0300

    1

--- components/content-service/pkg/git/git.go
+++ components/content-service/pkg/git/git.go
@@ -330,7 +330,7 @@ func (c *Client) Clone(ctx context.Context) (err error) {
 		log.WithError(err).Error("cannot create clone location")
 	}
 
-	args := []string{"--filter=blob:none", c.RemoteURI}
+	args := []string{"--filter=blob:none", "--also-filter-submodules", "--recurse-submodules", c.RemoteURI}
 
 	for key, value := range c.Config {
 		args = append(args, "--config")

8a1c6aa7d2bdc4f4526bdfd72e7253aefdbfab63
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2022-10-19 12:04:05 -0300

    Remove shiftfs module loader

--- install/installer/pkg/components/ws-daemon/daemonset.go
+++ install/installer/pkg/components/ws-daemon/daemonset.go
@@ -9,8 +9,6 @@ import (
 
 	"github.com/gitpod-io/gitpod/installer/pkg/cluster"
 	"github.com/gitpod-io/gitpod/installer/pkg/common"
-	config "github.com/gitpod-io/gitpod/installer/pkg/config/v1"
-	"github.com/gitpod-io/gitpod/installer/pkg/shiftfs"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -94,10 +92,6 @@ fi
 			},
 			SecurityContext: &corev1.SecurityContext{Privileged: pointer.Bool(true)},
 		},
-	}
-
-	if cfg.Workspace.Runtime.FSShiftMethod == config.FSShiftShiftFS {
-		initContainers = append(initContainers, shiftfs.Container(ctx))
 	}
 
 	podSpec := corev1.PodSpec{
@@ -358,8 +352,7 @@ fi
 					Labels: labels,
 					Annotations: common.CustomizeAnnotation(ctx, Component, common.TypeMetaDaemonset, func() map[string]string {
 						return map[string]string{
-							"seccomp.security.alpha.kubernetes.io/shiftfs-module-loader": "unconfined",
-							common.AnnotationConfigChecksum:                              configHash,
+							common.AnnotationConfigChecksum: configHash,
 						}
 					}),
 				},
